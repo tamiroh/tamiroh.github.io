28505aa5d0c4fac2d4b69216febcd317dbff3c89
tamiroh <tamiroh71@gmail.com>
2026-08-21 22:38:54 +0900

    Update

--- src/Main.elm
+++ src/Main.elm
@@ -3,6 +3,7 @@ module Main exposing (main)
 import Browser
 import Html exposing (Html)
 import Html.Attributes as Attr
+import Random
 import Set exposing (Set)
 import Svg exposing (Svg)
 import Svg.Attributes as SvgAttr
@@ -15,9 +16,10 @@ import Svg.Events
 
 main : Program () Model Msg
 main =
-    Browser.sandbox
+    Browser.element
         { init = init
         , update = update
+        , subscriptions = \_ -> Sub.none
         , view = view
         }
 
@@ -26,13 +28,27 @@ main =
 -- MODEL
 
 
+type alias Cell =
+    ( Int, Int )
+
+
+type Status
+    = Ready
+    | Playing
+    | Lost
+    | Won
+
+
 type alias Model =
-    Set ( Int, Int )
+    { mines : Set Cell
+    , revealed : Set Cell
+    , status : Status
+    }
 
 
-init : Model
-init =
-    Set.empty
+init : () -> ( Model, Cmd Msg )
+init _ =
+    ( { mines = Set.empty, revealed = Set.empty, status = Ready }, Cmd.none )
 
 
 
@@ -40,14 +56,94 @@ init =
 
 
 type Msg
-    = Clicked ( Int, Int )
+    = Clicked Cell
+    | MinesPlaced Cell (Set Cell)
 
 
-update : Msg -> Model -> Model
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         Clicked cell ->
-            Set.insert cell model
+            case model.status of
+                Ready ->
+                    ( model, Random.generate (MinesPlaced cell) (mineGenerator cell) )
+
+                Playing ->
+                    ( reveal cell model, Cmd.none )
+
+                Lost ->
+                    ( model, Cmd.none )
+
+                Won ->
+                    ( model, Cmd.none )
+
+        MinesPlaced cell mines ->
+            ( reveal cell { model | mines = mines, status = Playing }, Cmd.none )
+
+
+mineGenerator : Cell -> Random.Generator (Set Cell)
+mineGenerator safe =
+    let
+        candidates =
+            List.filter ((/=) safe) cells
+    in
+    Random.list (List.length candidates) (Random.float 0 1)
+        |> Random.map
+            (\keys ->
+                List.map2 Tuple.pair keys candidates
+                    |> List.sortBy Tuple.first
+                    |> List.take mineCount
+                    |> List.map Tuple.second
+                    |> Set.fromList
+            )
+
+
+reveal : Cell -> Model -> Model
+reveal cell model =
+    if Set.member cell model.mines then
+        { model | status = Lost }
+
+    else
+        let
+            revealed =
+                spread model.mines cell model.revealed
+        in
+        { model
+            | revealed = revealed
+            , status =
+                if Set.size revealed + mineCount == List.length cells then
+                    Won
+
+                else
+                    Playing
+        }
+
+
+spread : Set Cell -> Cell -> Set Cell -> Set Cell
+spread mines cell revealed =
+    if Set.member cell revealed then
+        revealed
+
+    else if adjacentMines mines cell == 0 then
+        List.foldl (spread mines) (Set.insert cell revealed) (neighbors cell)
+
+    else
+        Set.insert cell revealed
+
+
+adjacentMines : Set Cell -> Cell -> Int
+adjacentMines mines cell =
+    List.length (List.filter (\neighbor -> Set.member neighbor mines) (neighbors cell))
+
+
+neighbors : Cell -> List Cell
+neighbors ( column, row ) =
+    List.concatMap
+        (\columnOffset ->
+            List.map (\rowOffset -> ( column + columnOffset, row + rowOffset )) [ -1, 0, 1 ]
+        )
+        [ -1, 0, 1 ]
+        |> List.filter (\cell -> cell /= ( column, row ) && List.member cell cells)
 
 
 
@@ -65,6 +161,83 @@ view model =
         [ board model ]
 
 
+board : Model -> Svg Msg
+board model =
+    Svg.svg
+        [ SvgAttr.width (String.fromFloat boardSize)
+        , SvgAttr.height (String.fromFloat boardSize)
+        , SvgAttr.viewBox ("0 0 " ++ String.fromFloat boardSize ++ " " ++ String.fromFloat boardSize)
+        ]
+        (cellShapes model ++ gridLines ++ clickTargets model)
+
+
+cellShapes : Model -> List (Svg msg)
+cellShapes model =
+    case model.status of
+        Lost ->
+            List.map (\cell -> cellRect cell [ SvgAttr.fill ink ]) cells
+
+        _ ->
+            List.concatMap (openedCell model) cells
+
+
+openedCell : Model -> Cell -> List (Svg msg)
+openedCell model cell =
+    if Set.member cell model.revealed then
+        cellRect cell [ SvgAttr.fill ink ]
+            :: (case adjacentMines model.mines cell of
+                    0 ->
+                        []
+
+                    count ->
+                        [ label cell count ]
+               )
+
+    else
+        []
+
+
+label : Cell -> Int -> Svg msg
+label ( column, row ) count =
+    Svg.text_
+        [ SvgAttr.x (String.fromFloat (position column + spacing / 2))
+        , SvgAttr.y (String.fromFloat (position row + spacing / 2))
+        , SvgAttr.textAnchor "middle"
+        , SvgAttr.dominantBaseline "central"
+        , SvgAttr.fontFamily "monospace"
+        , SvgAttr.fontSize (String.fromFloat (spacing / 2))
+        , SvgAttr.fill paper
+        ]
+        [ Svg.text (String.fromInt count) ]
+
+
+clickTargets : Model -> List (Svg Msg)
+clickTargets model =
+    case model.status of
+        Lost ->
+            []
+
+        Won ->
+            []
+
+        _ ->
+            List.filterMap
+                (\cell ->
+                    if Set.member cell model.revealed then
+                        Nothing
+
+                    else
+                        Just
+                            (cellRect cell
+                                [ SvgAttr.fill "transparent"
+                                , SvgAttr.cursor "pointer"
+                                , Svg.Events.onClick (Clicked cell)
+                                ]
+                            )
+                )
+                cells
+
+
 
 -- BOARD
 
@@ -72,6 +245,11 @@ view model =
 cellCount : Int
 cellCount =
     8
+
+
+mineCount : Int
+mineCount =
+    10
 
 
 spacing : Float
@@ -84,17 +262,22 @@ margin =
     36
 
 
+ink : String
+ink =
+    "#363636"
+
+
+paper : String
+paper =
+    "#ffffff"
+
+
 boardSize : Float
 boardSize =
     spacing * toFloat cellCount + margin * 2
 
 
-lineIndices : List Int
-lineIndices =
-    List.range 0 cellCount
-
-
-cells : List ( Int, Int )
+cells : List Cell
 cells =
     let
         cellIndices =
@@ -103,45 +286,12 @@ cells =
     List.concatMap (\column -> List.map (Tuple.pair column) cellIndices) cellIndices
 
 
-ink : String
-ink =
-    "#363636"
-
-
 position : Int -> Float
 position index =
     margin + spacing * toFloat index
 
 
-board : Model -> Svg Msg
-board model =
-    Svg.svg
-        [ SvgAttr.width (String.fromFloat boardSize)
-        , SvgAttr.height (String.fromFloat boardSize)
-        , SvgAttr.viewBox ("0 0 " ++ String.fromFloat boardSize ++ " " ++ String.fromFloat boardSize)
-        ]
-        (filledCells model ++ gridLines ++ clickTargets)
-
-
-filledCells : Model -> List (Svg msg)
-filledCells model =
-    List.map (\cell -> cellRect cell [ SvgAttr.fill ink ]) (Set.toList model)
-
-
-clickTargets : List (Svg Msg)
-clickTargets =
-    List.map
-        (\cell ->
-            cellRect cell
-                [ SvgAttr.fill "transparent"
-                , SvgAttr.cursor "pointer"
-                , Svg.Events.onClick (Clicked cell)
-                ]
-        )
-        cells
-
-
-cellRect : ( Int, Int ) -> List (Svg.Attribute msg) -> Svg msg
+cellRect : Cell -> List (Svg.Attribute msg) -> Svg msg
 cellRect ( column, row ) attributes =
     Svg.rect
         (SvgAttr.x (String.fromFloat (position column))
@@ -161,7 +311,7 @@ gridLines =
             , line (position index) (position 0) (position index) (position cellCount)
             ]
         )
-        lineIndices
+        (List.range 0 cellCount)
 
 
 line : Float -> Float -> Float -> Float -> Svg msg

2374672088d4a97610d6bd79a0d99d947bfa28d5
tamiroh <tamiroh71@gmail.com>
2026-08-25 16:51:45 +0900

    Refactor

--- src/Dice.elm
+++ src/Dice.elm
@@ -1,12 +1,35 @@
-module Dice exposing (cells, view)
+module Dice exposing (view)
 
 import Geometry exposing (Position)
 import Svg exposing (Svg)
 import Svg.Attributes as SvgAttr
 
 
+view : Float -> Position -> String -> Int -> List (Svg msg)
+view span origin fill count =
+    List.map (dot span origin fill) (cells count)
 
--- DICE
+
+dot : Float -> Position -> String -> ( Int, Int ) -> Svg msg
+dot span ( x, y ) fill ( column, row ) =
+    Svg.circle
+        [ SvgAttr.cx (String.fromFloat (x + offset span column))
+        , SvgAttr.cy (String.fromFloat (y + offset span row))
+        , SvgAttr.r (String.fromFloat (radius span))
+        , SvgAttr.fill fill
+        , SvgAttr.stroke "none"
+        ]
+        []
+
+
+offset : Float -> Int -> Float
+offset span index =
+    span * (0.25 + 0.25 * toFloat index)
+
+
+radius : Float -> Float
+radius span =
+    span / 14
 
 
 cells : Int -> List ( Int, Int )
@@ -48,34 +71,3 @@ cells count =
 
         _ ->
             []
-
-
-
--- VIEW
-
-
-view : Float -> Position -> String -> Int -> List (Svg msg)
-view span origin fill count =
-    List.map (dot span origin fill) (cells count)
-
-
-dot : Float -> Position -> String -> ( Int, Int ) -> Svg msg
-dot span ( x, y ) fill ( column, row ) =
-    Svg.circle
-        [ SvgAttr.cx (String.fromFloat (x + offset span column))
-        , SvgAttr.cy (String.fromFloat (y + offset span row))
-        , SvgAttr.r (String.fromFloat (radius span))
-        , SvgAttr.fill fill
-        , SvgAttr.stroke "none"
-        ]
-        []
-
-
-offset : Float -> Int -> Float
-offset span index =
-    span * (0.25 + 0.25 * toFloat index)
-
-
-radius : Float -> Float
-radius span =
-    span / 14
